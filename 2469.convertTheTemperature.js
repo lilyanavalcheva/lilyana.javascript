f4fcb0cab3b92663c93ef18e9da5c3aa26371bb0
lilyanavalcheva <119426857+lilyanavalcheva@users.noreply.github.com>
2022-12-29 14:29:07 +0200

    updating the function

--- 2469.convertTheTemperature.js
+++ 2469.convertTheTemperature.js
@@ -1,5 +1,5 @@
-var convertTemperature = function(celsius) {
+var convertTemperature = function convertTemperature(celsius) {
     let kelvin =  celsius + 273.15
     let fahrenheit = celsius * 1.80 + 32.00
     return [kelvin, fahrenheit]
- }
+}
